7021528914675227a728ed952e50471adb4f99cd
Flusch <yavuzselim.guler@ogr.iuc.edu.tr>
2022-03-15 16:55:44 +0300

    necessary files created

---
+++ homeworkTwo/logo-odev2/Middlewares/AppVersionControllerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace logo_odev2.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class AppVersionControllerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AppVersionControllerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class AppVersionControllerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAppVersionControllerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AppVersionControllerMiddleware>();
+        }
+    }
+}
--- homeworkTwo/logo-odev2/Startup.cs
+++ homeworkTwo/logo-odev2/Startup.cs
@@ -1,15 +1,12 @@
+using logo_odev2.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
-namespace logo_odev2
+namespace logo_odev2API
 {
     public class Startup
     {
@@ -23,7 +20,12 @@ namespace logo_odev2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "logo_odev2", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,15 +34,13 @@ namespace logo_odev2
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "logo_odev2 v1"));
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
+            app.UseAppVersionControllerMiddleware();
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -48,9 +48,7 @@ namespace logo_odev2
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
