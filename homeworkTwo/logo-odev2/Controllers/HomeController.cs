796bcc579da1caac99a220b9527edb2431dd4288
Flusch <yavuzselim.guler@ogr.iuc.edu.tr>
2022-03-16 12:22:31 +0300

    filter added

---
+++ homeworkTwo/logo-odev2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace logo_odev2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Index([FromHeader] Version version)
+        {
+            Version ver = new Version();
+            ver.CompareTo(version);
+            return Ok();
+        }
+        
+    }
+}
---
+++ homeworkTwo/logo-odev2/Filters/SwaggerOperationFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+namespace logo_odev2.Filters
+{
+    public class SwaggerOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "app-version",
+                In = ParameterLocation.Header,
+                Required = true,
+                Schema = new OpenApiSchema
+                {
+                    Type = "String"
+                }
+            });
+        }
+    }
+}
