e3dbf66b3bb6d9e5a650266ec3e7b0b71baf9a64
Flusch <yavuzselim.guler@ogr.iuc.edu.tr>
2022-03-31 01:51:37 +0300

    added homework four

---
+++ homework4/First.App.DataAccess.EntityFramework/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using First.App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace First.App.DataAccess.EntityFramework.Configurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.ToTable("Companies").HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
---
+++ homework4/First.App.DataAccess.EntityFramework/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using First.App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace First.App.DataAccess.EntityFramework.Configurations
+{
+    internal class UserConfiguration
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users").HasKey(x => x.Id);
+            builder.Property(x => x.Username).IsRequired();
+            builder.Property(x => x.Password).IsRequired();
+        }
+    }
+}
